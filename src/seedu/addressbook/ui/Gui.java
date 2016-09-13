bce7b91dab8bbe1d7f07f7e2ab84d4becd9007bf
Bernard <bernardified@gmail.com>
2016-09-14 00:50:47 +0800

    Add Picture.png as app icon

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.Main;
+import javafx.scene.image.Image;
 
 import java.io.File;
 import java.io.IOException;
@@ -39,6 +40,7 @@ public class Gui {
         loader.setLocation(Main.class.getResource("ui" + File.separator + "mainwindow.fxml"));
         stage.setTitle(version);
         stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
+        stage.getIcons().add(new Image("file:src/seedu/addressbook/ui/Picture1.png"));
         stage.show();
         MainWindow mainWindow = loader.getController();
         mainWindow.setLogic(logic);
